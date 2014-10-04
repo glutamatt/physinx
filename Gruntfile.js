a5c2b36f071fac5f6ca1b712c9044c2459a369a0
Glutamatt <glutamatt@live.fr>
2014-10-05 00:44:11 +0200

    conf install

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,18 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    bower: { install : {} },
+    bower: { install : { options : { cleanTargetDir: true } } },
     concat: {
       options: {separator: ';'},
       dist: {
         src: ['bower_components/box2d.js/box2d.js', 'physics/module.js'],
-        dest: 'physics/box2d.js',
-      },
+        dest: 'physics/box2d.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-bower-task');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('default', ['bower', 'concat');
+  grunt.registerTask('default', ['bower', 'concat']);
 };
