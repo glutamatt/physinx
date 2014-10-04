a2c1da55ec6cd5075d85e498204cf1573c87dda1
Glutamatt <glutamatt@live.fr>
2014-10-05 01:30:53 +0200

    mathematical model live

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var box2d = require('./physics/box2d')
 
 var world = new box2d.b2World( new box2d.b2Vec2(0.0, -10.0) );
-console.log(world)
 var groundBody = world.CreateBody( new box2d.b2BodyDef() );
-console.log(groundBody)
+
+var bd = new box2d.b2BodyDef();
+bd.set_type(box2d.b2_dynamicBody);
+bd.set_position(new box2d.b2Vec2(0, 0));
+var body = world.CreateBody(bd);
+
+var step = function() {
+    world.Step(1/60, 3, 2);
+    console.log(body.GetPosition().get_y())
+}
+setInterval(step, 300);
