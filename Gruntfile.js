f261315c8f08aeaaac1b43e0f9c7f3b7d7cf9918
Glutamatt <glutamatt@live.fr>
2014-10-05 01:04:00 +0200

    config

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,13 @@ module.exports = function(grunt) {
         src: ['bower_components/box2d.js/box2d.js', 'physics/module.js'],
         dest: 'physics/box2d.js'
       }
-    }
+    },
+    clean: ["./lib"]
   });
 
   grunt.loadNpmTasks('grunt-bower-task');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', ['bower', 'concat']);
+  grunt.registerTask('default', ['bower', 'concat', 'clean']);
 };
