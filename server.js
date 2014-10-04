ebeb9b4e4f1b8fec5b1c1a0813d88116f4a1f484
Glutamatt <glutamatt@live.fr>
2014-10-05 00:51:49 +0200

    test the lib

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-console.log('hello')
-
 var box2d = require('./physics/box2d')
 
-console.log(typeof box2d)
+var world = new box2d.b2World( new box2d.b2Vec2(0.0, -10.0) );
+console.log(world)
+var groundBody = world.CreateBody( new box2d.b2BodyDef() );
+console.log(groundBody)
