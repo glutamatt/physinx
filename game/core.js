dbbb0ea6d155d21951fd4255be6abde8d041fa6b
Glutamatt <glutamatt@live.fr>
2015-04-25 21:16:03 +0200

    Make the debug WORKS !!!

--- game/core.js
+++ game/core.js
@@ -14,29 +14,31 @@ exports.setOnStep = function(callback) {
 
 exports.start = function() {
 
-    var world = new box2d.b2World( new box2d.b2Vec2(0.0, -5.0));
+    var world = new box2d.b2World(new box2d.b2Vec2(0.0, -10.0));
     onWorldCreated(world)
 
     var fixDef = new box2d.b2FixtureDef
     fixDef.density = 0.7
     fixDef.friction = 0.5
     fixDef.restitution = 0.2
+
     var bodyDef = new box2d.b2BodyDef
     bodyDef.set_type(box2d.b2_dynamicBody)
     var shape = new box2d.b2PolygonShape;
-    shape.SetAsBox(2, 2)
+    shape.SetAsBox(.5, .5)
     fixDef.set_shape(shape)
-    bodyDef.get_position().set_x(2)
+    bodyDef.get_position().set_x(5)
     bodyDef.get_position().set_y(0)
+    bodyDef.set_angle(Math.PI/3)
     var newbody = world.CreateBody(bodyDef)
-    newbody.CreateFixture(fixDef)
+    newbody.CreateFixture(fixDef)    
 
 	setInterval(function() {
-        world.Step(1/60, 3, 2);
+        world.Step(1/60, 10, 10);
         console.log(newbody.GetPosition().get_y())
         world.ClearForces()
         onStep(world)
-	}, 300);
+	}, 1000/30);
 
     return {box2d: box2d, world: world}
 }
--- game/web.js
+++ game/web.js
@@ -3,13 +3,23 @@ require('_process').stderr = {write: function(m){console.log(m)}}
 var core = require('./core')
 
 core.setOnStep(function(world) {
+    context.fillStyle = 'rgb(0,0,0)'
+    context.fillRect( 0, 0, canvas.width, canvas.height )
+    context.save()
+    context.scale(1,-1)
+    context.scale(30,30)
+    context.fillStyle = 'rgb(255,255,0)'
     world.DrawDebugData()
+    context.restore()
 })
+
 core.setOnWorldCreated(function(world) {
     global.canvas = document.getElementById("canvas");
     global.context = canvas.getContext( '2d' );
     global.Box2D = core.box2d
-    world.SetDebugDraw(getCanvasDebugDraw());
+    var debugDraw = getCanvasDebugDraw()
+    debugDraw.SetFlags(1)
+    world.SetDebugDraw(debugDraw)
 })
 
 core.start()
